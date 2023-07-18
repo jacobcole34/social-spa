71aa5a123c994d077ccf4876848186c7e1f8ba27
Jacob Cole <jacob.cole@gmail.com>
2023-07-17 20:56:28 -0400

    react-router

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
-import ThoughtList from '../components/ThoughtList';
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
+import ThoughtList from "../components/ThoughtList";
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -10,17 +10,20 @@ const Home = () => {
   console.log(thoughts);
 
   return (
-<main>
-  <div className="flex-row justify-space-between">
-    <div className="col-12 mb-3">
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-      )}
-    </div>
-  </div>
-</main>
+    <main>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
+      </div>
+    </main>
   );
 };
 
